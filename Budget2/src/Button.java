29741ab67962c45b8a5372d48760df32e396ecc2
RSutton16 <150315871+RSutton16@users.noreply.github.com>
2024-10-23 12:58:54 -0500

    modified:   .vscode/settings.json 	deleted:    Budget2/src/Button.java 	deleted:    Budget2/src/Frame.java 	deleted:    Budget2/src/Main.java 	deleted:    Budget2/src/Panel.java 	new file:   BudgetApp/src/Amount.class 	new file:   BudgetApp/src/Frame.class 	new file:   BudgetApp/src/Main.class 	new file:   BudgetApp/src/Panel1.class 	new file:   BudgetApp/src/User.class 	deleted:    Pong/src/Ball.java 	deleted:    Pong/src/Entity.java 	deleted:    Pong/src/Frame.java 	deleted:    Pong/src/Game.java 	deleted:    Pong/src/Main.java 	deleted:    Pong/src/Paddle.java 	deleted:    Pong/src/Panel.java 	modified:   Pong2/.project 	modified:   Pong2/bin/Ball.class 	modified:   Pong2/bin/Frame.class 	modified:   Pong2/bin/Game.class 	modified:   Pong2/bin/Input.class 	modified:   Pong2/bin/Panel.class 	new file:   Pong2/src/Ball.class 	new file:   Pong2/src/Constants.class new file:   Pong2/src/Entity.class 	new file:   Pong2/src/Frame.class 	new file:   Pong2/src/Game.class 	new file:   Pong2/src/Input.class 	new file:   Pong2/src/Main.class 	new file:   Pong2/src/Paddle.class 	new file:   Pong2/src/Panel.class 	new file:   Pong2/src/Type.class

--- Budget2/src/Button.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JButton;
-
-public class Button extends JButton{
-
-    public Button(int x, int y, int sizeX, int sizeY, Panel panel) {
-        this.setBounds(x, y, sizeX, sizeY);
-        panel.add(this);
-    }
-}
--- Pong/src/Paddle.java
+++
@@ -1,7 +0,0 @@
-public class Paddle extends Entity{
-    Paddle(float x, float y, float width, float height){
-        super(x, y, width, height);
-    }
-
-    
-}
